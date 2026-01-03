a00a4696fcffeffb0c8836533589c2459101e3b4
chiang0674 <75111470+chiang0674@users.noreply.github.com>
2026-01-03 12:36:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-
-// 不做任何快取，永遠走網路最新版本
-self.addEventListener("fetch", () => {});
+self.addEventListener("install",()=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",()=>{});
